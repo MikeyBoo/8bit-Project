d42cd60e3d1b313b23f28155a1d1113ae8a4569e
MikeyBoo <5800495+MikeyBoo@users.noreply.github.com>
2020-06-28 21:36:19 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This is a personal project where I'm building an 8-bit console using the book Designing Video Game Hardware in Verilog by Steven Hugg
+http://8bitworkshop.com/ using open source tools through apio https://github.com/FPGAwars/apio for the UPduino 3.0 which can be found here:
+https://www.tindie.com/products/tinyvision_ai/upduino-v30-low-cost-lattice-ice40-fpga-board/
+ 
+I used apio to init upduino21 and to verify and build binaries to make life easier: 
+"apio init --board upduino21", "apio verify" & "apio build"
+
+To program the UPduino 3.0, I use Zadig that comes with apio:
+use the command "apio drivers --ftdi-enable" to run Zadig ot install appropriate USB driver then
+use command "tinyprog -p hardware.bin" to program board
---
+++ top.v
@@ -0,0 +1,50 @@
+
+/*
+following book by Steven Hugg called Designing Video Game Hardware in Verilog
+http://8bitworkshop.com/ using open source tools through apio https://github.com/FPGAwars/apio for UPduino 3.0
+https://www.tindie.com/products/tinyvision_ai/upduino-v30-low-cost-lattice-ice40-fpga-board/
+steps taken: 
+use apio to init upduino21 and to verify and build binaries: 
+"apio init --board upduino21", "apio verify" & "apio build"
+use "apio drivers --ftdi-enable"
+use "tinyprog -p hardware.bin" to program board
+*/
+
+module test_hvsync_top(reset, hsync, vsync, rgb);
+
+  input reset;
+  output hsync, vsync;
+  output [2:0] rgb;
+  wire display_on;
+  wire [9:0] hpos;
+  wire [9:0] vpos; 
+
+
+  SB_HFOSC
+  #(
+    .CLKHF_DIV ("0b10")
+  ) my_osc (
+    .CLKHFPU (1'b1),
+    .CLKHFEN (1'b1),
+    .CLKHF   (clk) 
+  );
+
+
+  pll my_pll(.clock_in(clk), .clock_out(my_clk)); //radiant pll generator works a lot better - when it decides to work, that is
+
+  hvsync_generator hvsync_gen(
+    .clk(my_clk),
+    .reset(1'b0),
+    .hsync(hsync),
+    .vsync(vsync),
+    .display_on(display_on),
+    .hpos(hpos),
+    .vpos(vpos)
+  );
+
+  wire r = display_on && (((hpos&7)==0) || ((vpos&7)==0));
+  wire g = display_on && vpos[4];
+  wire b = display_on && hpos[4];
+  assign rgb = {b,g,r};
+
+endmodule
